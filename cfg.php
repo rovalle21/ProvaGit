e2ec45a7678b3fd23eeb23df0d2614da2549d921
rovalle21 <93399779+rovalle21@users.noreply.github.com>
2023-03-30 15:27:46 +0200

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+    $host="localhost"; 
+    $username="root"; 
+    $password="root"; 
+    $db_name="AmbulaTransit";
+    $db = mysqli_connect($host, $username, $password, $db_name); //or die("cannot connect");
+?>
